8b5ccfc3fb8c83448cf1118d99529decbbc92364
StevePro7 <steveprozune@hotmail.com>
2021-10-20 21:37:18 +0100

    no message

--- Homer/__sfx/banks/bank2.adb
+++ Homer/__sfx/banks/bank2.adb
@@ -1,2 +1,2 @@
 M:bank2
-S:G$right01_wav_pcmenc$0$0({20493}DA20493d,SC:U),D,0,0
+S:G$right01_wav_pcmenc$0$0({17367}DA17367d,SC:U),D,0,0
--- Homer/__sfx/banks/bank2.h
+++ Homer/__sfx/banks/bank2.h
@@ -1,4 +1,4 @@
 extern const unsigned char	right01_wav_pcmenc[];
-#define				right01_wav_pcmenc_size 20493
+#define				right01_wav_pcmenc_size 17367
 #define				right01_wav_pcmenc_bank 2
 
