87dc53ca33fa80908fd6492b6967832a3052f87e
StevePro7 <steveprozune@hotmail.com>
2021-10-20 20:52:37 +0100

    no message

---
+++ Homer/__sfx/banks/bank2.adb
@@ -0,0 +1,2 @@
+M:bank2
+S:G$right01_wav_pcmenc$0$0({8926}DA8926d,SC:U),D,0,0
---
+++ Homer/__sfx/scripts/bank2.bat
@@ -0,0 +1,18 @@
+echo start
+:: Setup.
+cd ../banks/bank2/raw
+pcmenc -rto 1 -dt1 12 -dt2 12 -dt3 423 right01.wav
+mv right01.wav.pcmenc ../
+
+:: Convert.
+cd ../..
+folder2c bank2 bank2 2
+
+:: Compile
+sdcc --debug -c --no-std-crt0 -mz80 --Werror --opt-code-speed --constseg BANK2 bank2.c
+
+:: Cleanup
+if exist "*.asm" del "*.asm" > nul; 
+if exist "*.lst" del "*.lst" > nul; 
+if exist "*.sym" del "*.sym" > nul
+echo -end-
