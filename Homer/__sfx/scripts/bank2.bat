4f71ff2c54a65dc57de056889f599550182e1925
StevePro7 <steveprozune@hotmail.com>
2021-10-20 21:04:03 +0100

    no message

--- Homer/__sfx/scripts/bank2.bat
+++ Homer/__sfx/scripts/bank2.bat
@@ -15,4 +15,6 @@ sdcc --debug -c --no-std-crt0 -mz80 --Werror --opt-code-speed --constseg BANK2 b
 if exist "*.asm" del "*.asm" > nul; 
 if exist "*.lst" del "*.lst" > nul; 
 if exist "*.sym" del "*.sym" > nul
+
+cd ../scripts
 echo -end-
