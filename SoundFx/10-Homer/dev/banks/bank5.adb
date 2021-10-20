1fe28b756177b7d57c92db1dfbc7966c68e434a4
StevePro7 <steveprozune@hotmail.com>
2021-10-20 22:02:01 +0100

    no message

--- SoundFx/10-Homer/dev/banks/bank5.adb
+++ SoundFx/10-Homer/dev/banks/bank5.adb
@@ -1,2 +1,2 @@
 M:bank5
-S:G$wrong01_wav_pcmenc$0$0({6014}DA6014d,SC:U),D,0,0
+S:G$wrong01_wav_pcmenc$0$0({6677}DA6677d,SC:U),D,0,0
--- SoundFx/10-Homer/dev/banks/bank5.h
+++ SoundFx/10-Homer/dev/banks/bank5.h
@@ -1,4 +1,4 @@
 extern const unsigned char	wrong01_wav_pcmenc[];
-#define				wrong01_wav_pcmenc_size 6014
+#define				wrong01_wav_pcmenc_size 6677
 #define				wrong01_wav_pcmenc_bank 5
 
