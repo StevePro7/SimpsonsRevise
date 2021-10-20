b8bc251c060578c125df8c80c399f065e1095955
StevePro7 <steveprozune@hotmail.com>
2021-10-20 22:25:25 +0100

    no message

--- Video/TitleScreen/dev/gfx.bat
+++ Video/TitleScreen/dev/gfx.bat
@@ -1,7 +1,7 @@
 cd ..
 cd gfx
 
-bmp2tile.exe raw\01.bmp -savetiles "splash (tiles).psgcompr" -removedupes -nomirror -planar -tileoffset 8 -savetilemap "splash (tilemap).stmcompr" -savepalette "splash (palette).bin" -fullpalette -exit
+bmp2tile.exe raw\title.bmp -savetiles "splash (tiles).psgcompr" -removedupes -nomirror -planar -tileoffset 8 -savetilemap "splash (tilemap).stmcompr" -savepalette "splash (palette).bin" -fullpalette -exit
 
 cd ..
 cd dev
--- Video/TitleScreen/dev/gfx.h
+++ Video/TitleScreen/dev/gfx.h
@@ -2,8 +2,8 @@ extern const unsigned char	splash__palette__bin[];
 #define				splash__palette__bin_size 16
 
 extern const unsigned char	splash__tilemap__stmcompr[];
-#define				splash__tilemap__stmcompr_size 75
+#define				splash__tilemap__stmcompr_size 196
 
 extern const unsigned char	splash__tiles__psgcompr[];
-#define				splash__tiles__psgcompr_size 2099
+#define				splash__tiles__psgcompr_size 7685
 
