09cb95cd269f2c95d2574e12a416c901296bb795
StevePro7 <steveprozune@hotmail.com>
2021-10-21 18:28:25 +0100

    no message

--- Video/ActorsView/dev/main.c
+++ Video/ActorsView/dev/main.c
@@ -5,9 +5,9 @@ void main( void )
 	devkit_SMS_init();
 	devkit_SMS_displayOff();
 	engine_asm_manager_clear_VRAM();
-	
+
+	engine_content_manager_load_font_tiles();
 	engine_content_manager_load_sprite_palette();
-	engine_content_manager_load_splash_screen();
 
 	devkit_SMS_displayOn();
 	for( ;; )
