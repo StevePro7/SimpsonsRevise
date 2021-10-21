017efa2ddf787345ae5b5d585a2cf55776b04a5d
StevePro7 <steveprozune@hotmail.com>
2021-10-21 18:46:19 +0100

    no message

--- Video/ActorsView/dev/build.bat
+++ Video/ActorsView/dev/build.bat
@@ -15,6 +15,7 @@ set /a _started=_hours*60*60*100+_min*60*100+_sec*100+_cs
 cd engine
 ::sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 asm_manager.c
 sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 content_manager.c
+sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 font_manager.c
 cd ..
 
 sdcc --debug -c -mz80 --opt-code-speed --peep-file peep-rules.txt --std-c99 main.c
@@ -52,6 +53,7 @@ banks\bank7.rel ^
 devkit/_sms_manager.rel ^
 engine/asm_manager.rel ^
 engine/content_manager.rel ^
+engine/font_manager.rel ^
 gfx.rel
 
 :: Execute
--- Video/ActorsView/dev/main.c
+++ Video/ActorsView/dev/main.c
@@ -6,9 +6,11 @@ void main( void )
 	devkit_SMS_displayOff();
 	engine_asm_manager_clear_VRAM();
 
-	//engine_content_manager_load_font_tiles();
-	//engine_content_manager_load_sprite_palette();
-	engine_content_manager_load_splash();
+	//engine_content_manager_load_splash();
+
+	engine_content_manager_load_font_tiles();
+	engine_content_manager_load_sprite_palette();
+	
 
 	devkit_SMS_displayOn();
 	for( ;; )
