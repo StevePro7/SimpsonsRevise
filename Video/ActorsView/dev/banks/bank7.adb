56fd375fe6e71d10239d16e26a0be8134d267c0b
StevePro7 <steveprozune@hotmail.com>
2021-10-21 18:34:35 +0100

    no message

---
+++ Video/ActorsView/dev/banks/bank7.adb
@@ -0,0 +1,4 @@
+M:bank7
+S:G$simpsons__palette__bin$0$0({16}DA16d,SC:U),D,0,0
+S:G$simpsons__tilemap__stmcompr$0$0({193}DA193d,SC:U),D,0,0
+S:G$simpsons__tiles__psgcompr$0$0({7783}DA7783d,SC:U),D,0,0
