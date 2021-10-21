6fa2d7310e228ebbfa1f4d3defaf72255d50d6cd
StevePro7 <steveprozune@hotmail.com>
2021-10-21 18:34:00 +0100

    no message

--- Video/ActorsView/dev/scripts/bank6.bat
+++ Video/ActorsView/dev/scripts/bank6.bat
@@ -1,20 +1,21 @@
-echo start
+cd ..
+cd banks
+cd bank6
+
 :: Setup.
-cd ../banks/bank6/raw
-pcmenc -rto 1 -dt1 12 -dt2 12 -dt3 423 wrong02.wav
-mv wrong02.wav.pcmenc ../
+bmp2tile.exe raw\splash.bmp -savetiles "splash (tiles).psgcompr" -removedupes -nomirror -planar -tileoffset 64 -savetilemap "splash (tilemap).stmcompr" -savepalette "splash (palette).bin" -fullpalette -exit
 
 :: Convert.
-cd ../..
+cd ..
 folder2c bank6 bank6 6
 
 :: Compile
 sdcc --debug -c --no-std-crt0 -mz80 --Werror --opt-code-speed --constseg BANK6 bank6.c
 
 :: Cleanup
-if exist "*.asm" del "*.asm" > nul; 
-if exist "*.lst" del "*.lst" > nul; 
+if exist "*.asm" del "*.asm" > nul
+if exist "*.lst" del "*.lst" > nul
 if exist "*.sym" del "*.sym" > nul
 
-cd ../scripts
-echo -end-
+cd ..
+cd scripts
