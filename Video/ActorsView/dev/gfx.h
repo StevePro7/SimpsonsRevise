446bff4b8665cb44b094e2f74776072f71ab0ab0
StevePro7 <steveprozune@hotmail.com>
2021-10-21 18:22:08 +0100

    no message

--- Video/ActorsView/dev/gfx.h
+++
@@ -1,9 +0,0 @@
-extern const unsigned char	splash__palette__bin[];
-#define				splash__palette__bin_size 16
-
-extern const unsigned char	splash__tilemap__stmcompr[];
-#define				splash__tilemap__stmcompr_size 75
-
-extern const unsigned char	splash__tiles__psgcompr[];
-#define				splash__tiles__psgcompr_size 2099
-
