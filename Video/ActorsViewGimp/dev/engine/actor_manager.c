a72a6e580f780566a9a45de18a60e0e70adf5a58
StevePro7 <steveprozune@hotmail.com>
2021-10-21 21:43:32 +0100

    no message

--- Video/ActorsViewGimp/dev/engine/actor_manager.c
+++ Video/ActorsViewGimp/dev/engine/actor_manager.c
@@ -1,33 +1,24 @@
 #include "actor_manager.h"
-//#include "font_manager.h"
 #include "../devkit/_sms_manager.h"
 #include "../object/actor_object.h"
-#include "../banks/bank2.h"
-#include "../banks/bank3.h"
 #include "../gfx.h"
 
 #define ACTOR_TILES_OFFSET		64
+#define ACTOR_TILES_TOTALS		4
+#define ACTOR_TILES_BANKED		2
 
 //void engine_actor_manager_draw( const unsigned char index )
 void engine_actor_manager_draw()
 {
-	const unsigned char index = 11;
+	const unsigned char index = 15;
+
 	const unsigned char *tileset = ( const unsigned char * ) actor_tileset[ index ];
 	const unsigned char *tilemap = ( const unsigned char * ) actor_tilemap[ index ];
 	const unsigned char *palette = ( const unsigned char * ) actor_palette[ index ];
-	//unsigned char banking = ( const unsigned char ) actor_banking[ index ];
+	unsigned char banking = index / ACTOR_TILES_TOTALS + ACTOR_TILES_BANKED;
 
-	// Magic number = 2 : base bank.
-	unsigned char banking = index / 4 + 2;
 	devkit_SMS_mapROMBank( banking );
-	//devkit_SMS_mapROMBank( 2 );
-	//////devkit_SMS_loadPSGaidencompressedTiles( actor00__tiles__psgcompr, ACTOR_TILES_OFFSET );
 	devkit_SMS_loadPSGaidencompressedTiles( tileset, ACTOR_TILES_OFFSET );
-	//////devkit_SMS_loadSTMcompressedTileMap( 0, 0, ( void * ) actor00__tilemap__stmcompr );
 	devkit_SMS_loadSTMcompressedTileMap( 0, 0, ( void * ) tilemap );
-	//////devkit_SMS_loadBGPalette( ( void * ) actor00__palette__bin );
 	devkit_SMS_loadBGPalette( ( void * ) palette );
-
-	//engine_font_manager_draw_data( rom, 10, 14 );
-	//engine_font_manager_draw_data( banking, 10, 14 );
 }
--- Video/ActorsViewGimp/dev/main.c
+++ Video/ActorsViewGimp/dev/main.c
@@ -12,12 +12,14 @@ void main(void)
 	devkit_SMS_VDPturnOnFeature( devkit_VDPFEATURE_HIDEFIRSTCOL() );
 
 	//engine_content_manager_load_splash();
+	
+	engine_content_manager_load_font_tiles();
 	//engine_content_manager_load_title();
-	//engine_content_manager_load_font_tiles();
 
 	engine_actor_manager_draw();
 	engine_content_manager_load_sprite_palette();
 
+	engine_font_manager_draw_text( "HELLO", 1, 1 );
 	devkit_SMS_displayOn();
 	for (;;)
 	{
