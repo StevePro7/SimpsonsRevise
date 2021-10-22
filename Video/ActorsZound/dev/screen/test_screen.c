35bcfc69f57f4e6566047c2a2559b2e3bd1c0be2
StevePro7 <steveprozune@hotmail.com>
2021-10-22 18:17:16 +0100

    no message

--- Video/ActorsZound/dev/screen/test_screen.c
+++ Video/ActorsZound/dev/screen/test_screen.c
@@ -1,19 +1,26 @@
 #include "test_screen.h"
-#include "../engine/actor_manager.h"
+//#include "../engine/actor_manager.h"
 #include "../engine/enum_manager.h"
 #include "../engine/font_manager.h"
+#include "../engine/input_manager.h"
 
 static unsigned char index = 14;
 
 void screen_test_screen_load()
 {
-	engine_actor_manager_init();
-	engine_actor_manager_draw( index );
+	//engine_actor_manager_init();
+	//engine_actor_manager_draw( index );
 
 	engine_font_manager_text( "TEST SCREEN...!!", 10, 3 );
 }
 
 void screen_test_screen_update( unsigned char *screen_type )
 {
+	unsigned char input = engine_input_manager_hold_down();
+	if( input )
+	{
+		engine_font_manager_text( "PRESS DOWN", 10, 5 );
+	}
+
 	*screen_type = screen_type_test;
 }
