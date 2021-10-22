b02bee0624db7514e8650eb3b69c12b433213358
StevePro7 <steveprozune@hotmail.com>
2021-10-22 20:13:51 +0100

    no message

--- Video/ActorsZound/dev/scripts/bank4.bat
+++ Video/ActorsZound/dev/scripts/bank4.bat
@@ -2,7 +2,7 @@ cd ..
 cd banks
 
 :: Compile
-sdcc --debug -c --no-std-crt0 -mz80 --Werror --opt-code-speed --constseg BANK5 bank5.c
+sdcc --debug -c --no-std-crt0 -mz80 --Werror --opt-code-speed --constseg BANK4 bank4.c
 
 :: Cleanup
 if exist "*.asm" del "*.asm" > nul
