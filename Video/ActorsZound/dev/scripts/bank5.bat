a5c2dbf5525e7f8490a3a36d5e87cb1274fa1818
StevePro7 <steveprozune@hotmail.com>
2021-10-22 20:13:00 +0100

    no message

--- Video/ActorsZound/dev/scripts/bank5.bat
+++ Video/ActorsZound/dev/scripts/bank5.bat
@@ -1,1 +1,13 @@
-echo bank5.txt
+cd ..
+cd banks
+
+:: Compile
+sdcc --debug -c --no-std-crt0 -mz80 --Werror --opt-code-speed --constseg BANK5 bank5.c
+
+:: Cleanup
+if exist "*.asm" del "*.asm" > nul
+if exist "*.lst" del "*.lst" > nul
+if exist "*.sym" del "*.sym" > nul
+
+cd ..
+cd scripts
