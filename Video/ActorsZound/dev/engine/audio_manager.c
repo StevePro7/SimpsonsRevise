2f4d7ebbeba7aafa2e48e6debfa541e77ec7e98b
StevePro7 <steveprozune@hotmail.com>
2021-10-22 18:26:28 +0100

    no message

--- Video/ActorsZound/dev/engine/audio_manager.c
+++ Video/ActorsZound/dev/engine/audio_manager.c
@@ -1,1 +1,44 @@
 #include "audio_manager.h"
+#include "../devkit/_snd_manager.h"
+#include "../psg.h"
+
+// IMPORTANT disable compiler warning 110
+#ifdef _CONSOLE
+#else
+#pragma disable_warning 196
+#endif
+
+#define sfx_cheat_psg			sfx_cheat_psg
+#define sfx_right_psg			sfx_right_psg
+#define sfx_wrong_psg			sfx_wrong_psg
+
+// Private helper functions.
+//static void play_music( unsigned char *mus, unsigned char bank );
+//static void play_music_norepeat( unsigned char *mus, unsigned char bank );
+static void play_sound( unsigned char *sfx );
+
+
+void engine_audio_manager_cheat()
+{
+	play_sound( ( unsigned char* ) sfx_cheat_psg );
+}
+void engine_audio_manager_right()
+{
+	play_sound( ( unsigned char* ) sfx_right_psg );
+}
+void engine_audio_manager_wrong()
+{
+	play_sound( ( unsigned char* ) sfx_wrong_psg );
+}
+
+
+static void play_sound( unsigned char *sfx )
+{
+	//struct_hack_object *ho = &global_hack_object;
+	//if( !ho->hack_sound )
+	//{
+	//	return;
+	//}
+
+	devkit_PSGSFXPlay( sfx, devkit_SFX_CHANNEL2() );
+}
--- Video/ActorsZound/dev/engine/audio_manager.h
+++ Video/ActorsZound/dev/engine/audio_manager.h
@@ -1,4 +1,8 @@
 #ifndef _AUDIO_MANAGER_H_
 #define _AUDIO_MANAGER_H_
 
+void engine_audio_manager_cheat();
+void engine_audio_manager_right();
+void engine_audio_manager_wrong();
+
 #endif//_AUDIO_MANAGER_H_
