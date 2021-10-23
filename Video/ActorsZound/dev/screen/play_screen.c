29612c93d38911dcf105355ea09ec323f067f7d1
StevePro7 <steveprozune@hotmail.com>
2021-10-23 21:35:41 +0100

    no message

--- Video/ActorsZound/dev/screen/play_screen.c
+++ Video/ActorsZound/dev/screen/play_screen.c
@@ -12,7 +12,7 @@ void screen_play_screen_init()
 
 void screen_play_screen_load()
 {
-	engine_font_manager_text( "PLAY SCREEN!!", 10, 2 );
+	engine_font_manager_text( "PLAY SCREEN..!!", 10, 2 );
 }
 
 void screen_play_screen_update( unsigned char *screen_type )
