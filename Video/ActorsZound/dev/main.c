49d9b4cba1111b6f23bdcc7265f228f8a6654d3c
StevePro7 <steveprozune@hotmail.com>
2021-10-23 19:08:45 +0100

    no message

--- Video/ActorsZound/dev/main.c
+++ Video/ActorsZound/dev/main.c
@@ -14,8 +14,8 @@ void main(void)
 	devkit_SMS_useFirstHalfTilesforSprites_True();
 	devkit_SMS_VDPturnOnFeature( devkit_VDPFEATURE_HIDEFIRSTCOL() );
 
-	//engine_content_manager_load_font();
-	//engine_content_manager_load_sprites();
+	engine_content_manager_load_font();
+	engine_content_manager_load_sprites();
 	//engine_content_manager_load_sprite_palette();	// TODO move to title after splash
 	engine_sound_manager_init();
 
