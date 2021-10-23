5c69cd5b9151d2f6aa9ec1ec4af6e765d1f43ab2
StevePro7 <steveprozune@hotmail.com>
2021-10-23 17:18:40 +0100

    no message

--- Video/ActorsZound/dev/scripts/bank15.bat
+++ Video/ActorsZound/dev/scripts/bank15.bat
@@ -3,9 +3,8 @@ cd banks
 ::cd bank15
 
 :: Convert.
-cd ..
-folder2c ..\bank15 bank15
-
+::cd ..
+folder2c bank15 bank15 15
 
 :: Compile
 sdcc --debug -c --no-std-crt0 -mz80 --Werror --opt-code-speed --constseg BANK15 bank15.c
