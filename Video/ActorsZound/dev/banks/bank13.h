4cd68095d86a52621a3f342dd590423baa8e6771
StevePro7 <steveprozune@hotmail.com>
2021-10-23 16:39:15 +0100

    no message

--- Video/ActorsZound/dev/banks/bank13.h
+++ Video/ActorsZound/dev/banks/bank13.h
@@ -1,7 +1,3 @@
-extern const unsigned char	music_psg[];
-#define				music_psg_size 7528
-#define				music_psg_bank 13
-
 extern const unsigned char	right03_wav_pcmenc[];
 #define				right03_wav_pcmenc_size 6598
 #define				right03_wav_pcmenc_bank 13
