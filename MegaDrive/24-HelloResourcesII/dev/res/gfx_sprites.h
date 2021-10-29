7ae5ae6730fd010e3e661a4f4ac9c6e825d4839f
StevePro7 <steveprozune@hotmail.com>
2021-10-29 17:56:17 +0100

    no message

---
+++ MegaDrive/24-HelloResourcesII/dev/res/gfx_sprites.h
@@ -0,0 +1,8 @@
+#ifndef _RES_GFX_SPRITES_H_
+#define _RES_GFX_SPRITES_H_
+
+extern const SpriteDefinition gfx_select;
+extern const SpriteDefinition gfx_right;
+extern const SpriteDefinition gfx_wrong;
+
+#endif // _RES_GFX_SPRITES_H_
