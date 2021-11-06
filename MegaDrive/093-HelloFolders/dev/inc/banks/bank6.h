beb2281731c17a26c4bf27b4104dfa5109f20745
StevePro7 <steveprozune@hotmail.com>
2021-11-06 16:49:28 +0000

    no message

--- MegaDrive/093-HelloFolders/dev/inc/banks/bank6.h
+++ MegaDrive/093-HelloFolders/dev/inc/banks/bank6.h
@@ -1,48 +1,48 @@
-extern const unsigned char	actor00__palette__bin[];
-#define				actor00__palette__bin_size 16
-#define				actor00__palette__bin_bank 6
-
-extern const unsigned char	actor00__tilemap__stmcompr[];
-#define				actor00__tilemap__stmcompr_size 85
-#define				actor00__tilemap__stmcompr_bank 6
-
-extern const unsigned char	actor00__tiles__psgcompr[];
-#define				actor00__tiles__psgcompr_size 2253
-#define				actor00__tiles__psgcompr_bank 6
-
-extern const unsigned char	actor01__palette__bin[];
-#define				actor01__palette__bin_size 16
-#define				actor01__palette__bin_bank 6
-
-extern const unsigned char	actor01__tilemap__stmcompr[];
-#define				actor01__tilemap__stmcompr_size 75
-#define				actor01__tilemap__stmcompr_bank 6
-
-extern const unsigned char	actor01__tiles__psgcompr[];
-#define				actor01__tiles__psgcompr_size 1820
-#define				actor01__tiles__psgcompr_bank 6
-
-extern const unsigned char	actor02__palette__bin[];
-#define				actor02__palette__bin_size 16
-#define				actor02__palette__bin_bank 6
-
-extern const unsigned char	actor02__tilemap__stmcompr[];
-#define				actor02__tilemap__stmcompr_size 93
-#define				actor02__tilemap__stmcompr_bank 6
-
-extern const unsigned char	actor02__tiles__psgcompr[];
-#define				actor02__tiles__psgcompr_size 2581
-#define				actor02__tiles__psgcompr_bank 6
-
-extern const unsigned char	actor03__palette__bin[];
-#define				actor03__palette__bin_size 16
-#define				actor03__palette__bin_bank 6
-
-extern const unsigned char	actor03__tilemap__stmcompr[];
-#define				actor03__tilemap__stmcompr_size 110
-#define				actor03__tilemap__stmcompr_bank 6
-
-extern const unsigned char	actor03__tiles__psgcompr[];
-#define				actor03__tiles__psgcompr_size 2929
-#define				actor03__tiles__psgcompr_bank 6
-
+//extern const unsigned char	actor00__palette__bin[];
+//#define				actor00__palette__bin_size 16
+//#define				actor00__palette__bin_bank 6
+//
+//extern const unsigned char	actor00__tilemap__stmcompr[];
+//#define				actor00__tilemap__stmcompr_size 85
+//#define				actor00__tilemap__stmcompr_bank 6
+//
+//extern const unsigned char	actor00__tiles__psgcompr[];
+//#define				actor00__tiles__psgcompr_size 2253
+//#define				actor00__tiles__psgcompr_bank 6
+//
+//extern const unsigned char	actor01__palette__bin[];
+//#define				actor01__palette__bin_size 16
+//#define				actor01__palette__bin_bank 6
+//
+//extern const unsigned char	actor01__tilemap__stmcompr[];
+//#define				actor01__tilemap__stmcompr_size 75
+//#define				actor01__tilemap__stmcompr_bank 6
+//
+//extern const unsigned char	actor01__tiles__psgcompr[];
+//#define				actor01__tiles__psgcompr_size 1820
+//#define				actor01__tiles__psgcompr_bank 6
+//
+//extern const unsigned char	actor02__palette__bin[];
+//#define				actor02__palette__bin_size 16
+//#define				actor02__palette__bin_bank 6
+//
+//extern const unsigned char	actor02__tilemap__stmcompr[];
+//#define				actor02__tilemap__stmcompr_size 93
+//#define				actor02__tilemap__stmcompr_bank 6
+//
+//extern const unsigned char	actor02__tiles__psgcompr[];
+//#define				actor02__tiles__psgcompr_size 2581
+//#define				actor02__tiles__psgcompr_bank 6
+//
+//extern const unsigned char	actor03__palette__bin[];
+//#define				actor03__palette__bin_size 16
+//#define				actor03__palette__bin_bank 6
+//
+//extern const unsigned char	actor03__tilemap__stmcompr[];
+//#define				actor03__tilemap__stmcompr_size 110
+//#define				actor03__tilemap__stmcompr_bank 6
+//
+//extern const unsigned char	actor03__tiles__psgcompr[];
+//#define				actor03__tiles__psgcompr_size 2929
+//#define				actor03__tiles__psgcompr_bank 6
+//
