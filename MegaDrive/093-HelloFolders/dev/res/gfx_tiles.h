50c6f7123164aa7fab0662a6fff225ea3b9f16e8
StevePro7 <steveprozune@hotmail.com>
2021-11-06 15:11:55 +0000

    no message

---
+++ MegaDrive/093-HelloFolders/dev/res/gfx_tiles.h
@@ -0,0 +1,13 @@
+#ifndef _RES_GFX_TILES_H_
+#define _RES_GFX_TILES_H_
+
+#ifdef _CONSOLE
+#include "_genesis.h"
+#else
+#include <genesis.h>
+#endif
+
+extern const Image gfx_splash;
+extern const Image gfx_simpsons;
+
+#endif // _RES_GFX_TILES_H_
