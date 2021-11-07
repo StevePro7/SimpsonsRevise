21660b192fbafc33529946ae74b9b1b26664e0c2
StevePro7 <steveprozune@hotmail.com>
2021-11-07 17:38:46 +0000

    no message

---
+++ MegaDrive/10-SimpsonsSpritesVH/dev/inc/main.h
@@ -0,0 +1,13 @@
+#ifndef __MAIN__
+#define __MAIN__
+
+#ifdef _CONSOLE
+#include "_genesis.h"
+#else
+#include <genesis.h>
+#endif
+
+//#include "gfx.h"
+#include "gfx_sprites.h"
+
+#endif//__MAIN__
---
+++ MegaDrive/10-SimpsonsSpritesVH/dev/src/main.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+int main()
+{
+	Sprite *sprite;
+	const SpriteDefinition *spriteDef = &gfx_right;
+	SPR_init( 0, 0, 0 );
+
+	unsigned short *data = NULL;
+	if( NULL != spriteDef->palette )
+	{
+		data = spriteDef->palette->data;
+	}
+	VDP_setPalette( PAL3, data );
+	sprite = SPR_addSprite( spriteDef, 100, 100, TILE_ATTR( PAL3, 0, FALSE, FALSE ) );
+	SPR_setPosition( sprite, 200, 100 );
+
+	//VDP_drawText( "Hello Simpsons", 10, 13 );
+	while( 1 )
+	{
+		SPR_update();
+		VDP_waitVSync();
+	}
+
+	return 0;
+}
