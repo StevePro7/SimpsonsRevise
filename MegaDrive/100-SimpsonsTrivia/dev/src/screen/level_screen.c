cb7aa5e41c67997bcff870f17216924f680e8608
StevePro7 <steveprozune@hotmail.com>
2021-11-29 16:07:33 +0000

    fix bug

--- MegaDrive/100-SimpsonsTrivia/dev/src/screen/level_screen.c
+++ MegaDrive/100-SimpsonsTrivia/dev/src/screen/level_screen.c
@@ -1,13 +1,18 @@
 #include "level_screen.h"
+#include "../engine/debug_manager.h"
 #include "../engine/enum_manager.h"
-#include "../engine/font_manager.h"
-#include "../engine/global_manager.h"
-#include "../engine/quiz_manager.h"
 #include "../engine/random_manager.h"
 #include "../engine/select_manager.h"
 
 void screen_level_screen_load()
 {
+	// Process this here just before quiz starts
+	// Why?  Otherwise may cause music to "drag"
+	engine_random_manager_init();
+	engine_random_manager_load();
+
+	engine_select_manager_load_quiz();
+	engine_debug_manager_init();
 }
 
 void screen_level_screen_update( unsigned char *screen_type )
--- MegaDrive/100-SimpsonsTrivia/dev/src/screen/number_screen.c
+++ MegaDrive/100-SimpsonsTrivia/dev/src/screen/number_screen.c
@@ -1,27 +1,12 @@
 #include "number_screen.h"
 #include "../engine/actor_manager.h"
-#include "../engine/debug_manager.h"
 #include "../engine/enum_manager.h"
-#include "../engine/font_manager.h"
-#include "../engine/global_manager.h"
 #include "../engine/graphics_manager.h"
-#include "../engine/random_manager.h"
-#include "../engine/select_manager.h"
 
 void screen_number_screen_load()
 {
 	engine_graphics_manager_clear_full();
 	engine_actor_manager_load();
-
-	// TODO should be in level_screen.c
-	// Process this here just before quiz starts
-	// Why?  Otherwise may cause music to "drag"
-	engine_random_manager_init();
-
-	engine_random_manager_load();
-	engine_select_manager_load_quiz();
-
-	engine_debug_manager_init();
 }
 
 void screen_number_screen_update( unsigned char *screen_type )
